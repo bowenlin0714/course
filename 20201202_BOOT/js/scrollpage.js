fa3a63e63abe390fd2a7dd59df2a295d8cd4eb90
bowen125125 <bowen125125@gmail.com>
2020-12-07 16:32:30 +0800

    相簿

---
+++ 20201202_BOOT/js/scrollpage.js
@@ -0,0 +1,17 @@
+$('.navbar-brand, #main_menu a[href^="#"],#go_top_page a').on('click', function(e) {
+  // js
+  // let $target = $(this.getAttribute('href'))
+  // jq
+  let $target = $($(this).attr('href'))
+  if($target.length) {
+    e.preventDefault();
+    $('html,body').stop().animate(
+     {
+      scrollTop: $target.offset().top
+     },
+     {
+      duration: 2000,
+      easing: 'easeInOutBack'
+     })
+  }
+})
