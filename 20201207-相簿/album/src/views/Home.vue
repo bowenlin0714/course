ce756adeec9b48b2055fc446dd216fcdf3339f0a
bowen125125 <bowen125125@gmail.com>
2020-12-08 16:33:54 +0800

    album

---
+++ 20201207-相簿/album/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  h1.text-center 歡迎使用線上相簿
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
--- 20201207-相簿/back/controllers/albums.js
+++ 20201207-相簿/back/controllers/albums.js
@@ -175,9 +175,10 @@ export const user = async (req, res) => {
   }
 
   try {
-    const result = albums.find({ user: req.params.user })
+    const result = await albums.find({ user: req.params.user })
     res.status(200).send({ success: true, message: '', result })
   } catch (error) {
+    console.log(error)
     res.status(500).send({ success: false, message: '伺服器錯誤' })
   }
 }
